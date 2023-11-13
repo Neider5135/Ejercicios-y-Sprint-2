081620f93e2854dddbe1ce2132752082b40fc375
Dnunez <neider5135@gmail.com>
2023-11-13 16:50:33 -0500

    Avance bucles

---
+++ Bucles/main.js
@@ -0,0 +1,198 @@
+/*
+//1
+let num1 = parseInt(prompt("Ingrese un número para obtener su tabla de multiplicación en la consola"))
+let r= 1
+while (r<11) {
+    res = num1 * r
+    console.log(res)
+    r+=1
+}
+*/
+/*
+//2
+let num2 = parseInt(prompt("Ingrese un número"))
+let acom=[]
+while (parseInt(num2) != 0) {
+    if(!(isNaN (num2))){
+        acom.push (num2)
+    }
+    console.log(acom)
+    console.table (acom)
+    num2 = parseInt(prompt("Ingrese un número"))
+}
+alert ("Los números ingresados fueron " + acom)
+*/
+/*
+//3
+let inten = 1
+let nIncog = 60
+let nInten = parseInt(prompt("El número misterioso está entre el 1 y el 100, ¿podrás adivinarlo?"))
+
+while (nIncog != nInten) {
+    inten ++
+    if (isNaN(nInten) || nInten > 100 || nInten < 1) {
+        alert ("Acabas de desperdiciar un intento")
+    }else if (nInten < nIncog) {
+        alert ("El número misterioso es mayor")
+    } else {
+        alert ("El número misterioso es menor")
+    }
+    nInten = parseInt(prompt("Ingresa un número entre el 1 y el 100"))
+}
+alert ("Ganaste!! el número misterioso era " + nIncog + " y el número de intentos fue " + inten)
+*/
+/*
+//4
+let div = 0;
+let nIng = parseInt(prompt("Ingresa tu número para saber si es primo"));
+
+if (isNaN(nIng) || nIng <= 0) {
+  alert("Ingresaste un carácter inválido");
+} else {
+  for (let i = 1; i <= nIng; i++) {
+    if (nIng % i == 0) {
+      div++;
+    }
+  }
+  if (div != 2) {
+    alert(nIng + " No es primo");
+  } else {
+    alert(nIng + " Es primo");
+  }
+}
+*/
+/*
+//5
+let divi = 0;
+let divisores = []
+let nIngr = parseInt(prompt("Ingresa tu número para descubrir todos sus divisores"));
+
+if (isNaN(nIngr) || nIngr <= 0) {
+  alert("Ingresaste un carácter inválido");
+} else {
+  for (let i = 1; i <= nIngr; i++) {
+    if (nIngr % i == 0) {
+      divi++;
+      divisores.push (i)
+    }
+  }
+  if (divi == 1) {
+    alert("El único divisor de " + nIngr + " es: " + divisores);
+  } else {
+    alert(divisores + " son los divisores de " + nIngr);
+  }
+}
+*/
+/*
+//6
+let arrayP = [15, "j", 58, 25, "Jaja XD", "No te pases de lanza", 8, 0, "Vaca", 10]
+
+for (let i = 0; i < arrayP.length; i++) {
+    console.log (arrayP[i])    
+}
+*/
+/*
+//7
+let arrayP2 = [15, 2, 58, 25, 5, 6, 8, 0, 9, 10]
+
+for (let i = 0; i < arrayP2.length; i++) {
+    console.log ((arrayP2[i]) * 2)    
+}
+*/
+/*
+//8
+let familia = [
+    persona1 = {
+      nombre: "Juanito",
+      apellido: "Perez",
+      edad: 50,
+      papelFamiliar: "Padre",
+    },
+    persona2 = {
+      nombre: "Juanita",
+      apellido: "De Perez",
+      edad: 47,
+      papelFamiliar: "Madre",
+    },
+    persona3 = {
+      nombre: "Juanito. Jr",
+      apellido: "Perez",
+      edad: 21,
+      papelFamiliar: "Hijo menor"
+    },
+    persona4 = {
+      nombre: "Juanita",
+      apellido: "Perez",
+      edad: 23,
+      papelFamiliar: "Hija del medio"
+    },
+    persona5 = {
+      nombre: "Juancho",
+      apellido: "Perez",
+      edad: 25,
+      papelFamiliar: "Hijo mayor"
+    },
+  ]
+  for (let i = 0; i < familia.length; i++) {
+    alert ("Mi nombre es " + familia[i].nombre + " " + familia[i].apellido + " tengo " + familia[i].edad + " años y en la familia soy " + familia[i].papelFamiliar)
+  }
+*/
+/*
+//9
+let arrayP3 = [15, 2, 58, 25, 5, 6, 8, 0, 9, 10]
+
+for (let i = 0; i < arrayP3.length; i++) {
+    if ((arrayP3[i]) % 2 == 0) {
+        console.log (arrayP3[i])  
+    }  
+}
+*/
+
+//10
+
+
+/*
+//11
+let arrayP4 = [15, 2, 58, 25, 5, 6, 8, 0, 9, 102]
+
+let masGr = 0
+
+for (let i = 0; i < arrayP4.length; i++) {
+    if ((arrayP4[i]) > masGr) {
+        masGr = arrayP4 [i]
+    }  
+}
+alert ("El número más grande de la lista es: " + masGr)
+*/
+/*
+//12
+let arrayP4 = [15, 2, 58, 25, 5, 6, 8, 3, 9, 1]
+
+let masPe = 0
+
+for (let i = 0; i < arrayP4.length; i++) {
+    if ((arrayP4[i]) < masPe || i == 0) {
+        masPe = arrayP4 [i]
+    }  
+}
+alert ("El número más pequeño de la lista es: " + masPe)
+*/
+/*
+//13
+do {
+    jugador1 = parseInt(prompt("Turno del jugador 1. Piedra: 1, Papel: 2, Tijeras: 3"))
+    jugador2 = parseInt(prompt("Turno del jugador 2. Piedra: 1, Papel: 2, Tijeras: 3"))
+    if (isNaN(jugador1) || isNaN(jugador2) ||  jugador1 < 1 || jugador1 > 3 || jugador2 < 1 || jugador2 > 3){
+        alert ("Se ingresó valor inválido")
+    } else if ((jugador1 == 1 && jugador2 == 2) || (jugador1 == 2 && jugador2 == 3) || (jugador1 == 3 && jugador2 == 1)){
+        alert ("Gana jugador 2")
+    } else if (jugador1 == jugador2) {
+        alert ("Empate!!")
+    } else {
+        alert ("Gana jugador 1")
+    }
+}
+while (jugador1 == jugador2)
+*/
+
+//14
